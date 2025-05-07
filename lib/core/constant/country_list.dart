316cd40087aff735e0d06708704ad92939170acd
ibrammkdr <ibrahimkadirkls@gmail.com>
2025-05-07 19:36:35 +0300

    c

---
+++ lib/core/constant/country_list.dart
@@ -0,0 +1,19 @@
+class AppText {
+  final title = "Countries";
+  final searchHint = "Search For Country Or City";
+  final connectText = "Connecting Time";
+  final bottom1Text = "Countries";
+  final bottom2Text = "Disconnect";
+  final bottom3Text = "Settings";
+  final freeLocation = "Free Location (3)";
+  final download = "Download:";
+  final upload = "Upload:";
+  final stealth = "Stealth";
+  final vipSucces = "Congratulations!";
+  final vipUnSucces = "Information";
+  final vipSucMessage = "You are now VIP!";
+  final vipUnSucMessage = "VIP membership canceled.";
+  final unVipTitle = "Access denied";
+  final unVipSubtile = "You need to be a VIP to use this feature.";
+  final close = "Close";
+}
